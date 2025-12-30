e2fc2ed9ca138df943431bb4521a399c52de2b60
Vasylyna Bizniakova <vbiznyakova@gmail.com>
2025-12-30 17:19:13 +0100

    initial commit for 03tut

---
+++ 13TUT/controllers/authController.js
@@ -0,0 +1,73 @@
+const usersDB = {
+  users: require('../model/users.json'),
+  setUsers: function (data) {
+    this.users = data;
+  },
+};
+
+const bcrypt = require('bcrypt');
+
+const jwt = require('jsonwebtoken');
+const fsPromises = require('fs').promises;
+const path = require('path');
+
+const handleLogin = async (req, res) => {
+  const { user, pwd } = req.body;
+  if (!user || !pwd)
+    return res
+      .status(400)
+      .json({ message: 'Username and passwrod are required' });
+
+  const foundUser = usersDB.users.find((person) => person.username === user);
+  console.log(usersDB);
+  console.log(user);
+
+  if (!foundUser) return res.sendStatus(401); //Unauthorized
+  //evaluate password
+  const match = await bcrypt.compare(pwd, foundUser.password);
+  if (match) {
+    const roles = Object.values(foundUser.roles);
+    //create JWTs(normal and refresh)
+    // 1st arg = payload
+    //Payload is the actual data being transmitted inside a request, message, or token. In JWT, the payload contains claims such as user identity and token metadata, and it is Base64-encoded but not encrypted.
+    // no need to send the role is refresh token, only access token is ok to save this, frontend should take care of saving the role
+    const accessToken = jwt.sign(
+      {
+        UserInfo: {
+          username: foundUser.username,
+          roles: roles,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: '30s' } // TODO change for prod to 5 - 15 min
+    );
+
+    const refreshToken = jwt.sign(
+      { username: foundUser.username },
+      process.env.REFRESH_TOKEN_SECRET,
+      { expiresIn: '1d' } // TODO change for prod to 5 - 15 min
+    );
+    //Saving refreshToken with current user
+    const otherUsers = usersDB.users.filter(
+      (person) => person.username !== foundUser.username
+    );
+    const currentUser = { ...foundUser, refreshToken };
+    usersDB.setUsers([...otherUsers, currentUser]);
+    await fsPromises.writeFile(
+      path.join(__dirname, '..', 'model', 'users.json'),
+      JSON.stringify(usersDB.users)
+    );
+    res.cookie('jwt', refreshToken, {
+      // httpOnly cookie are not acessed by js, its not 100% secure but its much secure
+      httpOnly: true,
+      sameSite: 'None',
+      secure: true,
+      maxAge: 24 * 60 * 60 * 1000, //1 day
+    });
+    res.json({ accessToken });
+  } else {
+    res.sendStatus(401);
+  }
+};
+
+module.exports = { handleLogin };
